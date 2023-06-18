2bfc7fc7ada1c1c02bc0c63e903ffb8c7f54f851
Andrea-Som <97748745+Andrea-Som@users.noreply.github.com>
2023-06-18 16:15:49 -0400

    Update BlackScholesProcess.py

--- stochastic_processes/BlackScholesProcess.py
+++ stochastic_processes/BlackScholesProcess.py
@@ -51,47 +51,46 @@ class BlackScholesProcess:
 			path = sample_path.value()
 			value[i, :] = np.array(path[0])
 		return value                                  
-
+	
 class HestonProcess:
-    def __init__(self, s0=None, v0=None, kappa=None, theta=None, sigma=None, rho=None,
+	def __init__(self, s0=None, v0=None, kappa=None, theta=None, sigma=None, rho=None,
                  risk_free=None, dividend=None, day_count=None, seed=0):
-        self.s0 = s0
-        self.v0 = v0
-        self.kappa = kappa
-        self.theta = theta
-        self.sigma = sigma
-        self.rho = rho
-        self.risk_free = risk_free
-        self.dividend = dividend
-        self.day_count = day_count
-        self.seed = seed
+        	self.s0 = s0
+        	self.v0 = v0
+        	self.kappa = kappa
+        	self.theta = theta
+        	self.sigma = sigma
+        	self.rho = rho
+        	self.risk_free = risk_free
+        	self.dividend = dividend
+        	self.day_count = day_count
+        	self.seed = seed
 
-    def get_process(self, calculation_date=ql.Date.todaysDate()):
-        spot_handle = ql.QuoteHandle(ql.SimpleQuote(self.s0))
-        volatility_handle = ql.QuoteHandle(ql.SimpleQuote(np.sqrt(self.v0)))
-        risk_free_handle = ql.QuoteHandle(ql.SimpleQuote(self.risk_free))
-        dividend_handle = ql.QuoteHandle(ql.SimpleQuote(self.dividend))
+    	def get_process(self, calculation_date=ql.Date.todaysDate()):
+        	spot_handle = ql.QuoteHandle(ql.SimpleQuote(self.s0))
+        	volatility_handle = ql.QuoteHandle(ql.SimpleQuote(np.sqrt(self.v0)))
+        	risk_free_handle = ql.QuoteHandle(ql.SimpleQuote(self.risk_free))
+        	dividend_handle = ql.QuoteHandle(ql.SimpleQuote(self.dividend))
 
-        flat_ts = ql.YieldTermStructureHandle(ql.FlatForward(calculation_date, self.risk_free, self.day_count))
-        dividend_yield = ql.YieldTermStructureHandle(ql.FlatForward(calculation_date, self.dividend, self.day_count))
+        	flat_ts = ql.YieldTermStructureHandle(ql.FlatForward(calculation_date, self.risk_free, self.day_count))
+        	dividend_yield = ql.YieldTermStructureHandle(ql.FlatForward(calculation_date, self.dividend, self.day_count))
 	
-	ql.Settings.instance().evaluationDate = calculation_date
+		ql.Settings.instance().evaluationDate = calculation_date
 	
-        heston_process = ql.HestonProcess(flat_ts, dividend_yield, spot_handle, volatility_handle,
-                                          self.kappa, self.theta, self.sigma, self.rho)
+        	heston_process = ql.HestonProcess(flat_ts, dividend_yield, spot_handle, volatility_handle,self.kappa, self.theta, self.sigma, self.rho)
 	
-        return heston_process
+        	return heston_process
 
-    def gen_path(self, length=None, time_step=None, num_paths=None):
-        rng = ql.GaussianRandomSequenceGenerator(
-            ql.UniformRandomSequenceGenerator(time_step, ql.UniformRandomGenerator(self.seed)))
-        seq = ql.GaussianMultiPathGenerator(self.get_process(), np.linspace(0, length, time_step + 1), rng, False)
+    	def gen_path(self, length=None, time_step=None, num_paths=None):
+        	rng = ql.GaussianRandomSequenceGenerator(
+            	ql.UniformRandomSequenceGenerator(time_step, ql.UniformRandomGenerator(self.seed)))
+        	seq = ql.GaussianMultiPathGenerator(self.get_process(), np.linspace(0, length, time_step + 1), rng, False)
 
-        value = np.zeros((num_paths, time_step + 1))
+        	value = np.zeros((num_paths, time_step + 1))
 
-        for i in trange(num_paths):
-            sample_path = seq.next()
-            path = sample_path.value()
-            value[i, :] = np.array(path[0])
+        	for i in trange(num_paths):
+            		sample_path = seq.next()
+            		path = sample_path.value()
+            		value[i, :] = np.array(path[0])
 
-        return value
+        	return value
